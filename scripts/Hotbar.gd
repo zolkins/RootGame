6b4afac2afaa925200bc8f57d0ffb4793594e476
CreperDN <creperdn.game@gmail.com>
2023-02-02 18:32:36 +0200

    ver 1.0

--- scripts/Hotbar.gd
+++ scripts/Hotbar.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 const SlotClass = preload("res://scripts/Slot.gd")
-onready var inventory_slots = $Items/Texture/ScrollContainer/GridContainer
+onready var inventory_slots = $Items/Texture/GridContainer
 var holding_item = null
 
 func _ready():
@@ -12,19 +12,17 @@ func _ready():
 func slot_gui_input(event: InputEvent, slot: SlotClass):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT && event.pressed:
-			if holding_item != null:
-				if !slot.item:
-					holding_item = null
-					
-				else:	
-						var temp_item = slot.item
-						temp_item.global_position = event.global_position
-						holding_item = temp_item
-						holding_item.queue_free()
-						holding_item = null
-			
-			elif slot.item:
-				holding_item = slot.item
+			if holding_item == null:
+				holding_item = slot.item.duplicate()
+				add_child(holding_item)
+				holding_item.global_position = event.global_position
+				holding_item.global_position = get_global_mouse_position()
+			else: 
+				remove_child(holding_item)
+				var temp_item = slot.item.duplicate()
+				add_child(temp_item)
+				temp_item.global_position = event.global_position
+				holding_item = temp_item
 				holding_item.global_position = get_global_mouse_position()
 
 func _input(event):
@@ -34,4 +32,8 @@ func _input(event):
 
 func _on_Button_pressed():
 	$Items.set_visible(!$Items.is_visible())
+	if !$Items.is_visible():
+		$Button.set_position(Vector2(0, 300))
+	else:
+		$Button.set_position(Vector2(120, 300))
 	pass # Replace with function body.
