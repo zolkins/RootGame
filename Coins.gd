df7799bdffc96abbeefd9094a2606b1a125630fd
CreperDN <creperdn.game@gmail.com>
2023-02-02 16:47:44 +0200

    ver 0.2

---
+++ Coins.gd
@@ -0,0 +1,8 @@
+extends Label
+
+func _ready():
+	text = str(Global.coins)
+	pass # Replace with function body.
+
+
+
--- scripts/Hotbar.gd
+++ scripts/Hotbar.gd
@@ -14,29 +14,17 @@ func slot_gui_input(event: InputEvent, slot: SlotClass):
 		if event.button_index == BUTTON_LEFT && event.pressed:
 			if holding_item != null:
 				if !slot.item:
-					slot.putIntoSlot(holding_item)
 					holding_item = null
 					
-				else:
-					if holding_item.item_name != slot.item.item_name:	
+				else:	
 						var temp_item = slot.item
-						slot.pickFromSlot()
 						temp_item.global_position = event.global_position
-						slot.putIntoSlot(holding_item)
 						holding_item = temp_item
-					else:
-						var stack_size = int(JsonData.item_data[slot.item.item_name]["StackSize"])
-						var able_to_add = stack_size - slot.item.item_quantity
-						if able_to_add >= holding_item.item_quantity:
-							slot.item.add_item_quantity(holding_item.item_quantity)
-							holding_item.queue_free()
-							holding_item = null
-						else:
-							slot.item.add_item_quantity(able_to_add)
-							holding_item.decrease_item_quantity(able_to_add)
-			else:
+						holding_item.queue_free()
+						holding_item = null
+			
+			elif slot.item:
 				holding_item = slot.item
-				slot.pickFromSlot()
 				holding_item.global_position = get_global_mouse_position()
 
 func _input(event):
