e028aedb31832a512182d3be2f12a7d9388663c6
zolkins <57059372+zolkins@users.noreply.github.com>
2023-02-03 22:56:04 +0200

    add music

--- scripts/singleton/global.gd
+++ scripts/singleton/global.gd
@@ -4,6 +4,7 @@ onready var zoom_speed = Vector2(.05,.05)
 onready var coins = 100
 onready var is_phone = false
 onready var is_near = false
+onready var music_volume = AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), 40)
 
 func _ready():
 	TranslationServer.set_locale(languge)
