ce550d1683e072b6e40002a02e3cd7adae1930e5
zolkins <57059372+zolkins@users.noreply.github.com>
2023-02-05 03:19:01 +0200

    add sfx cash

--- scenes/int_shop_menu.gd
+++ scenes/int_shop_menu.gd
@@ -7,15 +7,19 @@ func _ready():
 
 func _on_lop_buy_pressed():
 	if (Global.coins - Global.shovel_x1x1_price) > 0 && Global.usable["Shovel1x1"] < 30:
-		$sfx.stream = preload("res://resources/mp3/sfx/any_in_settings.mp3")
+		$sfx.stream = preload("res://resources/mp3/sfx/cash.mp3")
 		$sfx.play()
 		Global.coins -= Global.shovel_x1x1_price
 		Global.usable["Shovel1x1"] += 5
-
+	else:
+		$sfx.stream = preload("res://resources/mp3/sfx/remove.wav")
+		$sfx.play()
 
 func _on_can_buy_pressed():
 	if (Global.coins - Global.can_x1x1_price) > 0:
-		$sfx.stream = preload("res://resources/mp3/sfx/any_in_settings.mp3")
+		$sfx.stream = preload("res://resources/mp3/sfx/cash.mp3")
 		$sfx.play()
 		Global.coins -= Global.can_x1x1_price
-	
+	else:
+		$sfx.stream = preload("res://resources/mp3/sfx/remove.wav")
+		$sfx.play()
--- scripts/portShop.gd
+++ scripts/portShop.gd
@@ -19,13 +19,19 @@ func _on_BurakSlider_value_changed(value):
 	$BurakCounter.text = str($BurakSlider.value) + "/$" + str($BurakSlider.value*Global.burak_price)
 
 func _on_PotatoButton_pressed():
+	$sfx.stream = preload("res://resources/mp3/sfx/cash.mp3")
+	$sfx.play()
 	Global.coins += $PotatoSlider.value*Global.potato_price 
 	Global.potato_counter -= $PotatoSlider.value
 
 func _on_CarrotButton_pressed():
+	$sfx.stream = preload("res://resources/mp3/sfx/cash.mp3")
+	$sfx.play()
 	Global.coins += $CarrotSlider.value*Global.carrot_price
 	Global.carrot_counter -=  $CarrotSlider.value
 
 func _on_BurakButton_pressed():
+	$sfx.stream = preload("res://resources/mp3/sfx/cash.mp3")
+	$sfx.play()
 	Global.coins += $BurakSlider.value*Global.burak_price
 	Global.burak_counter -=  $BurakSlider.value
