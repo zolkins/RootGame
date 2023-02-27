2d34f36341b833b5dbd133bf1e8d48f6d735a42f
zolkins <57059372+zolkins@users.noreply.github.com>
2023-02-28 01:30:05 +0200

    zoom on phone if Global.is_phone (djoystick)

--- scenes/Camera2D.gd
+++ scenes/Camera2D.gd
@@ -6,10 +6,11 @@ var zoom_speed = Global.zoom_speed
 var des_zoom = zoom
 
 func _process(delta):
-	zoom = lerp(zoom, des_zoom, .2)
+	if !Global.is_phone:
+		zoom = lerp(zoom, des_zoom, .2)
 
 func _input(event):
-	if event is InputEventMouseButton:
+	if event is InputEventMouseButton and !Global.is_phone:
 		if event.is_pressed():
 			if event.button_index == BUTTON_WHEEL_UP:
 				if des_zoom > zoom_min:
@@ -17,3 +18,35 @@ func _input(event):
 			if event.button_index == BUTTON_WHEEL_DOWN:
 				if des_zoom < zoom_max:
 					des_zoom += zoom_speed
+
+# for phone
+var min_zoom = .1
+var max_zoom = 1.5
+var zoom_sensitivity = 10
+var zoom_speed_p = 0.02
+var events = {}
+var last_drag_distance = 0
+
+
+func _unhandled_input(event):
+	if Global.is_phone:
+		if event is InputEventScreenTouch:
+			if event.pressed:
+				events[event.index] = event
+			else:
+				events.erase(event.index)
+	if Global.is_phone:
+		if event is InputEventScreenDrag:
+			events[event.index] = event
+			if events.size() == 2:
+				var keys = events.keys()
+				var key1 = keys[0]
+				var key2 = keys[1]
+				var touch1 = events[key1].position
+				var touch2 = events[key2].position
+				var drag_distance = touch1.distance_to(touch2)
+				if abs(drag_distance - last_drag_distance) > zoom_sensitivity:
+					var new_zoom = (1 + zoom_speed_p) if drag_distance < last_drag_distance else (1 - zoom_speed_p)
+					new_zoom = clamp(zoom.x * new_zoom, min_zoom, max_zoom)
+					zoom = Vector2.ONE * new_zoom
+					last_drag_distance = drag_distance
